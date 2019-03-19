2617b45c674bb4950aa92258e4e112dd0f15a666
Bongani Ndlovu <Bongani.N@explore-datascience.net>
2019-03-19 13:55:21 +0200

    Updating...

--- mypackage/recursion.py
+++ mypackage/recursion.py
@@ -5,40 +5,28 @@ Created on Mon Mar 18 23:20:01 2019
 @author: shin
 """
 def sum_array(array):
-    
-'''Return sum of all items in array'''    
+
     if len(array) == 0:
         return(0)
     else:
-        return(array[0] + sum_array(array[1:]))
-    
+        return array[0] + sum_array(array[1:])
+
 def fibonacci(n):
-    
-'''Return nth term in fibonacci sequence'''
-    
-    if n < 2:
-        return(n)
-    else:
-        return(fibonacci(n - 1) + fibonacci(n - 2))
-        
-def factorial(n):
-    
-    '''Return n!'''
-    
     if n == 0:
+        return(0)
+    elif n == 1 | n == 2:
         return(1)
     else:
-        return n * factorial(n - 1)
-    
-def reverse(word):
+        return(fibonacci(n - 1) + fibonacci(n - 2))
 
-'''Return word in reverse'''
-    
+def factorial(n):
+    if n == 0 | n == 1:
+        return(1)
+    else:
+        return(n * factorial(n - 1))
+
+def reverse(word):
     if word == "":
         return(word)
     else:
-        return(word[-1] + reverse(word[:-1]))    
-    
-
-
-        
+        word[-1] + reverse(word[:-1])
