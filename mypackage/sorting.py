d25b52b10dc09fd8c5dc4a45da8d3c14a5a13e71
Bongani Ndlovu <Bongani.N@explore-datascience.net>
2019-03-19 15:07:41 +0200

    Holy diver!

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -1,83 +1,48 @@
 def bubble_sort(items):
-    """ Implementation of bubble sort """
-    out = items.copy() # in place protection on items
-    for i in range(len(out)):
-        for j in range(len(out)-1-i):
-            if out[j] > out[j+1]:
-                out[j], out[j+1] = out[j+1], out[j]     # Swap!
-
-    return out
-
-def merge(A, B):
-    new_list = []
-    while len(A) > 0 and len(B) > 0:
-        if A[0] < B[0]:
-            new_list.append(A[0])
-            A.pop(0)
-        else:
-            new_list.append(B[0])
-            B.pop(0)
-
-    if len(A) == 0:
-        new_list = new_list + B
-    if len(B) == 0:
-        new_list = new_list + A
-
-    return new_list
+    '''Return array of items, sorted in ascending order'''
+    output = items
+    for i in range(len(output)):
+        for j in range(len(output)-i-1):
+            if output[j] > output[j+1]:
+                output[j], output[j+1] = output[j+1], output[j]
+    return output
 
 
 def merge_sort(items):
+    '''Return array of items, sorted in ascending order'''
+    if len(items)> 1:
+        mid_point = len(items)//2
 
-    len_i = len(items)
-    if len_i == 1:
-        return items
+        list1 = merge_sort(items[:mid_point])
+        list2 = merge_sort(items[mid_point:])
 
-    mid_point = int(len_i / 2)
-    i1 = merge_sort(items[:mid_point])
-    i2 = merge_sort(items[mid_point:])
+        i = j = k = 0
 
-    return merge(i1, i2)
+        while (len(list1)>i) and (len(list2)>j):
+            if list1[i] < list2[j]:
+                items[k] = list1[i]
+                i += 1
+            else:
+                items[k] = list2[j]
+                j +=1
+            k += 1
+
+        while len(list1) > i:
+            items[k] = list1[i]
+            i += 1
+            k += 1
+
+        while len(list2) > j:
+            items[k] = list2[j]
+            j += 1
+            k += 1
+
+    return items
+
 
 def quick_sort(items):
-    """
-    the quick sort algorithm takes in an unsorted list of numbers.
-    returns a list in ascending order.
-
-    Parameters
-    ----------
-    items : list
-        list of unordered numbers
-    index: int, optional
-        index number at which to choose the split value
-        default set to the last item in the input list
-
-    Returns
-    -------
-    list
-        list of elements in items in ascending order
-    """
-    index=-1
-    len_i = len(items)
-
-    if len_i <= 1:
-        # Logic Error
-        # identified with test run [1,5,4,3, 2, 6, 5, 4, 3, 8, 6, 5, 3, 1]
-        # len <= 1
+    '''Return array of items, sorted in ascending order'''
+    if len(items) <= 1:
         return items
-
-    pivot = items[index]
-    small = []
-    large = []
-    dup = []
-    for i in items:
-        if i < pivot:
-            small.append(i)
-        elif i > pivot:
-            large.append(i)
-        elif i == pivot:
-            dup.append(i)
-
-    small = quick_sort(small)
-    large = quick_sort(large)
-
-    return small + dup + large
+    else:
+        return quick_sort([i for i in items[1:] if i < items[0]]) + [items[0]] + quick_sort([i for i in items[1:] if i >= items[0]])
